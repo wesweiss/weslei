80dd4c71e947558a5e71fd5a0dac38ea5397b4ea
wesweiss <wesweiss@outlook.com>
2019-02-13 03:31:01 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const   express = require('express'),
+        app = express(),
+        dotenv = require('dotenv'),
+        bodyParser = require('body-parser'),
+        mongoose = require('mongoose'),
+        expressSanitizer = require('express-sanitizer'),
+        methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(expressSanitizer());
+app.use(express.static(__dirname + '/public'))
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname +'/views');
+
+dotenv.config();
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+
+// ROUTE
+app.get('/', (req, res) => res.render('home'));
+
+app.listen(process.env.PORT_APP, process.env.IP, () => console.log('Server Ok'));
---
+++ assets-angular/img/readme.txt
@@ -0,0 +1,8 @@
+Photo by Green Chameleon on Unsplash
+https://unsplash.com/photos/s9CC2SKySJM
+
+Photo by Émile Perron on Unsplash
+https://unsplash.com/photos/xrVDYZRGdw4
+
+Photo by Bench Accounting on Unsplash
+https://unsplash.com/photos/C3V88BOoRoM
