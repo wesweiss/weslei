d388325a28bbef8a61eb502b4b1bc62ed81cfd9f
wesweiss <wesweiss@outlook.com>
2019-02-21 03:49:21 +0000

    mongoose comentado momentaneamente

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const   express = require('express'),
         app = express(),
         dotenv = require('dotenv'),
         bodyParser = require('body-parser'),
-        mongoose = require('mongoose'),
+        // mongoose = require('mongoose'),
         expressSanitizer = require('express-sanitizer'),
         methodOverride = require('method-override'),
         Portugues = require('./public/assets/js/pt-br'),
@@ -17,8 +17,8 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname +'/views');
 
 dotenv.config();
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+// mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
 
 // ROUTE
 app.get('/', (req, res) => res.render('home', {language:Portugues}));
