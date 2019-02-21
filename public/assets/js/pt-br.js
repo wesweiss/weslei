93f9a013e8e942a67065fda831b3bde06f81f557
wesweiss <wesweiss@outlook.com>
2019-02-21 15:54:04 +0000

    correção título

--- public/assets/js/pt-br.js
+++ public/assets/js/pt-br.js
@@ -2,7 +2,7 @@ const Portugues = {
     sobre: "sobre",
     portfolio: "portfólio",
     contato: "contato",
-    titulo1: "desenvolvimento web",
+    titulo1: "desenvolvimento de aplicações web",
     titulo2: "análise de sistemas",
     rodape: "WESLEI - 2019 - direitos reservados"
 }
