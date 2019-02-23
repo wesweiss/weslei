54b1f28a93f8ed87208d5795cd519cc1edc0406a
wesweiss <wesweiss@outlook.com>
2019-02-23 17:21:24 +0000

    correção box texto aviso em breve

--- public/assets/js/en-us.js
+++ public/assets/js/en-us.js
@@ -4,7 +4,9 @@ const English = {
     contato: "contact",
     titulo1: "web development",
     titulo2: "systems analysis",
-    rodape: "WESLEI - 2019 - all rights reserved"
+    rodape: "WESLEI - 2019 - all rights reserved",
+    emBreve: "under construction... comeback soon"
 }
 
 module.exports = English;
+// module.exports is NODE, SO everytime you change anything here, you've got to reload server
--- public/assets/js/pt-br.js
+++ public/assets/js/pt-br.js
@@ -4,7 +4,9 @@ const Portugues = {
     contato: "contato",
     titulo1: "desenvolvimento de aplicações web",
     titulo2: "análise de sistemas",
-    rodape: "WESLEI - 2019 - direitos reservados"
+    rodape: "WESLEI - 2019 - direitos reservados",
+    emBreve: "em construção... retorne em breve"
 }
 
 module.exports = Portugues;
+// module.exports is NODE, SO everytime you change anything here, you've got to reload server
